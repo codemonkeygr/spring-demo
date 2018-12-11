8f171fcba5f8972bba8605f13b4d662338794651
CodeMonkey <codemonkey.gr@gmail.com>
2018-12-11 22:04:29 +0100

    Added controllers and Thymeleaf templates

---
+++ src/main/java/gr/verdant/springdemo/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package gr.verdant.springdemo.controllers;
+
+import gr.verdant.springdemo.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
